2d0d019f0d1623dea023b222b41dfe58a8a64a58
GhostOfIcarus <alysha.van.etten@hotmail.nl>
2023-10-31 19:08:09 +0100

    rooster overzicht

--- BuurtBoerApp/Views/Schedule_Overview.tsx
+++ BuurtBoerApp/Views/Schedule_Overview.tsx
@@ -1,26 +1,120 @@
 // HomeScreen.js
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Alert, Image, Text, TouchableOpacity, KeyboardAvoidingView, ScrollView   } from 'react-native';
 
-const HomeScreen = () => {
+interface WeekOverviewScreenProps {
+  navigation: any;
+}
+
+const WeekOverviewScreen = (props: WeekOverviewScreenProps) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the Home Screen!</Text>
-      <Button title="Go to Login" onPress={() => console.log('Navigate to Login')} />
-    </View>
+
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.top_bar_div}>
+        <View style={styles.img_div}>
+        <Image
+            source={require('./img/buurtboer_logo_no_texto.png')}
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.centered_text_square}>
+          <Text style={styles.top_text}>Rooster overzicht</Text>
+        </View>
+      </View>
+
+      <View style={styles.padding}>
+        <View style={styles.schedule_div}>
+          <View style={styles.centered_text_square}>
+              <Text style={styles.centered_text}>U heeft uw Rooster voor deze week nog niet ingevuld.</Text>
+          </View>
+
+          <TouchableOpacity style={styles.buttons}>
+            <Text style={{ color: 'white', textAlign: 'center' }}>Vul Rooster in</Text>
+          </TouchableOpacity>
+
+        </View>
+      </View>
+      
+    </ScrollView>
   );
 };
 
+
 const styles = StyleSheet.create({
-  container: {
+  container: 
+  {
+    flexGrow: 1,
+    justifyContent: 'center',
+    backgroundColor: '#D9D9D9',
+  },
+  top_bar_div:
+  {
+    flex: 1,
+    width: '100%',
+    height: 100,
+    justifyContent: 'center',
+    backgroundColor: '#099F91',
+    shadowColor: 'black',
+    shadowRadius: 10,
+  },
+  img_div: 
+  {
+    height: 100,
+  },
+  image: {
+    backgroundColor: '#099F91',
+  },
+  schedule_div: 
+  {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 25,
+    marginHorizontal: 20,
+    marginVertical: 250,
+    backgroundColor: '#fff',
+    shadowColor: 'black',
+    shadowRadius: 10,
   },
-  text: {
-    fontSize: 20,
+  centered_text_square: 
+  {
+    justifyContent: 'center',
+    alignItems: 'center',
     marginBottom: 20,
+  },
+  padding:
+  {
+    padding: 25,
+  },
+  centered_text: 
+  { color: 'black', 
+  fontWeight: '600', 
+  fontSize: 20, 
+  textAlign: 'center', 
+  },
+  top_text:
+  {
+    color: 'white', 
+    textAlign: 'center', 
+    fontSize: 30, 
+    fontWeight: 'bold', 
+    paddingLeft: 125,
+  },
+  buttons: 
+  {
+    backgroundColor: '#F9834C',
+    color: 'white',
+    fontWeight: '600',
+    padding: 10,
+    borderRadius: 10,
+    marginVertical: 5,
+    marginHorizontal: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 5, height: 20 },
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
+    elevation: 5, // For Android
   },
 });
 
-export default HomeScreen;
+export default WeekOverviewScreen;
