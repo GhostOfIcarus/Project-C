2b6e54e884a8ecc3041b5bba37baea6bbb34ee0f
JoeriDekker <joeridekker2002@gmail.com>
2023-10-31 19:32:46 +0100

    navbar fix?!?

--- BuurtBoerApp/Views/Schedule_Overview.tsx
+++ BuurtBoerApp/Views/Schedule_Overview.tsx
@@ -8,18 +8,19 @@ interface WeekOverviewScreenProps {
 
 const WeekOverviewScreen = (props: WeekOverviewScreenProps) => {
   return (
-
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.top_bar_div}>
-        <View style={styles.img_div}>
-        <Image
-            source={require('./img/buurtboer_logo_no_texto.png')}
-            style={styles.image}
-          />
-        </View>
-        <View style={styles.centered_text_square}>
-          <Text style={styles.top_text}>Rooster overzicht</Text>
-        </View>
+          <View style={styles.leftContent}>
+            <Image
+              source={require('./img/buurtboer_logo_no_texto.png')}
+              style={styles.image}
+            />
+          </View>
+          <View style={styles.rightContent}>
+            <View style={styles.centered_text_square}>
+              <Text style={styles.top_text}>Rooster overzicht</Text>
+            </View>
+          </View>
       </View>
 
       <View style={styles.padding}>
@@ -47,22 +48,38 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#D9D9D9',
   },
-  top_bar_div:
-  {
-    flex: 1,
+  top_bar_div: {
+    flexDirection: 'row', // Use row direction for horizontal layout
     width: '100%',
-    height: 100,
+    height: 105,
     justifyContent: 'center',
     backgroundColor: '#099F91',
     shadowColor: 'black',
     shadowRadius: 10,
+    marginTop: -20,
   },
-  img_div: 
-  {
-    height: 100,
+  top_text: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 30,
+    marginTop: 10,
+    fontWeight: 'bold',
+    width: '100%'
+  },
+  leftContent: {
+    paddingLeft: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rightContent: {
+    flex: 1, // Takes 1/2 of the available space
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   image: {
     backgroundColor: '#099F91',
+    height: 75,
+    width: 75,
   },
   schedule_div: 
   {
@@ -92,14 +109,7 @@ const styles = StyleSheet.create({
   fontSize: 20, 
   textAlign: 'center', 
   },
-  top_text:
-  {
-    color: 'white', 
-    textAlign: 'center', 
-    fontSize: 30, 
-    fontWeight: 'bold', 
-    paddingLeft: 125,
-  },
+  
   buttons: 
   {
     backgroundColor: '#F9834C',
