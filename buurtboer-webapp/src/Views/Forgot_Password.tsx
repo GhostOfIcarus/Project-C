8316ff83d69d5f55c75d0aebc2be538a6190b9d4
JoeriDekker <joeridekker2002@gmail.com>
2023-11-13 19:06:28 +0100

    helped with css import fix

--- buurtboer-webapp/src/Views/Forgot_Password.tsx
+++ buurtboer-webapp/src/Views/Forgot_Password.tsx
@@ -1,19 +1,29 @@
 import logo from './img/buurtboer_logo.png';
-import './Stylesheets/Forgot_Password.css';
+import styling from './Stylesheets/Forgot_Password.module.css';
 
 function Forgot_Password() {
   return (
     <>
-        <div className="container">
-            <p>wachtwoord vergeten</p>
-            <input type="email" placeholder="Email" /> 
-            <a href="Change_Password.tsx"><button>verstuur</button></a>
+        <div className={styling.container}>
+          <div className='row'>
+            <div className='col-6'>
+              <div>
+                <img className={styling.sideImage} src={logo} alt="buurtboerLogo" />
+              </div>
+            </div>
+            <div className='col-6'>
+              <p>wachtwoord vergeten</p>
+              <input type="email" placeholder="Email" /> 
+              <a href="Change_Password.tsx"><button>verstuur</button></a>
+            </div>
+          </div>
+            
         </div>
-        <div>
-            <img id="sideImage" src={logo} alt="buurtboerLogo" />
-        </div>
+        
     </>
   );
 }
+
+
 
 export default Forgot_Password;
