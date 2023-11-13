d8a2a18cd0b09462da5c849d375f252d0dbc8fbe
hesitantronin <roninvanegdom@outlook.com>
2023-11-13 19:11:47 +0100

    Trust the process bro

--- buurtboer-webapp/src/Views/Login.tsx
+++ buurtboer-webapp/src/Views/Login.tsx
@@ -2,24 +2,24 @@ import { useLoginController } from '../Controllers/LoginController';
 import { Link } from 'react-router-dom';  
 import logo from './img/buurtboer_logo.png'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Stylesheets/Login.css';
+import styles from './Stylesheets/Login.module.css';
 
 export function Login() {
   const { isSubmitted, renderErrorMessage, handleSubmit } = useLoginController();
 
   const renderForm = (
     <>
-      <div className='title'>Inloggen</div>
+      <div className={styles.title}>Inloggen</div>
       <form onSubmit={handleSubmit}>
         <input type='email' placeholder='Email' name='Email' required />
         {renderErrorMessage('Email')}
         <input type='password' placeholder='Wachtwoord' name='Pass' required />
         <Link to="/Forgot_Password" className='forgot-password'>Wachtwoord vergeten?</Link>
-        <div className='button-box'>
-          <button className='button'>Login</button>
+        <div className={styles['button-box']}>
+          <button className={styles['button']}>Login</button>
           <p>OF</p>
-          <button className='button'>Login met Google</button>
-          <button className='button'>Login met Microsoft</button>
+          <button className={styles['button']}>Login met Google</button>
+          <button className={styles['button']}>Login met Microsoft</button>
         </div>
       </form>
     </>
@@ -27,14 +27,14 @@ export function Login() {
 
   return (
     <>
-      <div className='Body'></div>
-      <div className="container">
+      <div className={styles.Body}></div>
+      <div className='container'>
         <div className='row'>
           <div className='col-lg-6 login-form'>
-            {isSubmitted ? <div className='logged-in'>Je bent ingelogd!</div> : renderForm}
+            {isSubmitted ? <div className={styles['logged-in']}>Je bent ingelogd!</div> : renderForm}
           </div>
           <div className='col-lg-6 image-box'>
-            <img src={logo} alt="Buurtboer Logo" className='Buurtboerlogo' />
+            <img src={logo} alt="Buurtboer Logo" className={styles.Buurtboerlogo} />
           </div>
         </div>
       </div>
