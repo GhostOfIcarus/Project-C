9c4d59d07b4cd7eeaedaf0e79763a43e028366cf
hesitantronin <roninvanegdom@outlook.com>
2023-11-30 13:42:44 +0100

    Updated LocalDb script

--- LocalDatabase/InitDb.js
+++ LocalDatabase/InitDb.js
@@ -12,41 +12,104 @@ const connection = {
 const pgp = pgPromise();
 const db = pgp(connection);
 
-async function createDatabase() {
-  try {
-    // Create the database if it doesn't exist
-    await db.none(`CREATE DATABASE IF NOT EXISTS ${connection.database}`);
+// async function createDatabase() {
+//   try {
+//     // Create the database if it doesn't exist
+//     await db.none(`CREATE DATABASE IF NOT EXISTS ${connection.database}`);
 
-    console.log(`Database '${connection.database}' created or already exists.`);
-  } catch (error) {
-    console.error('Error creating database:', error.message);
-  }
-}
+//     console.log(`Database '${connection.database}' created or already exists.`);
+//   } catch (error) {
+//     console.error('Error creating database:', error.message);
+//   }
+// }
 
-async function createTable() {
+async function createTables() {
   try {
     await db.none(`
-      CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        age INTEGER NOT NULL
+      CREATE TABLE IF NOT EXISTS Employee (
+        ID SERIAL PRIMARY KEY,
+        First_Name VARCHAR(255) NOT NULL,
+        Last_Name VARCHAR(255) NOT NULL,
+        Email VARCHAR(255) NOT NULL UNIQUE,
+        Password VARCHAR(64) NOT NULL,
+        KeepSchedule BOOLEAN NOT NULL
+      );
+
+      CREATE TABLE IF NOT EXISTS Company (
+        ID SERIAL PRIMARY KEY,
+        Admin_First_Name VARCHAR(255) NOT NULL,
+        Admin_Last_Name VARCHAR(255) NOT NULL,
+        Company_Name VARCHAR(255) NOT NULL,
+        Full_Schedule BOOLEAN NOT NULL,
+        Email VARCHAR(255) NOT NULL UNIQUE,
+        Password VARCHAR(64) NOT NULL
+      );
+
+      CREATE TABLE IF NOT EXISTS Schedule (
+        ID SERIAL PRIMARY KEY,
+        Week_Number INTEGER NOT NULL,
+        Monday BOOL NOT NULL,
+        Tuesday BOOL NOT NULL,
+        Wednesday BOOL NOT NULL,
+        Thursday BOOL NOT NULL,
+        Friday BOOL NOT NULL,
+        Saturday BOOL NOT NULL,
+        Sunday BOOL NOT NULL
+      );
+
+      CREATE TABLE IF NOT EXISTS ScheduleFromEmployee (
+        ID SERIAL PRIMARY KEY,
+        Schedule_ID INTEGER NOT NULL,
+        Company_ID INTEGER NOT NULL,
+        FOREIGN KEY (Schedule_ID) REFERENCES Schedule(ID),
+        FOREIGN KEY (Company_ID) REFERENCES Company(ID)
+      );      
+      
+      CREATE TABLE IF NOT EXISTS EmployeesInCompany (
+        ID SERIAL PRIMARY KEY,
+        Employee_ID INTEGER NOT NULL,
+        Company_ID INTEGER NOT NULL,
+        FOREIGN KEY (Employee_ID) REFERENCES Employee(ID),
+        FOREIGN KEY (Company_ID) REFERENCES Company(ID)
+      );
+
+      CREATE TABLE IF NOT EXISTS SuperAdmin (
+        ID SERIAL PRIMARY KEY,
+        First_Name VARCHAR(255) NOT NULL,
+        Last_Name VARCHAR(255) NOT NULL,
+        Email VARCHAR(255) NOT NULL UNIQUE,
+        Password VARCHAR(64) NOT NULL
       );
     `);
 
-    console.log('Table "users" created or already exists.');
+    console.log('Tables created or already exist.');
   } catch (error) {
-    console.error('Error creating table "users":', error.message);
+    console.error('Error creating tables:', error.message);
   }
 }
 
 async function insertTestData() {
   try {
-    // Insert some sample data into your table
+    // Write your insert queries here
     const insertDataQuery = `
-      INSERT INTO users (name, age)
-      VALUES
-        ('Drik-Jan', 99),
-        ('Alessandro', 25);
+      INSERT INTO Employee (First_Name, Last_Name, Email, Password, KeepSchedule)
+      VALUES ('John', 'Doe', 'john.dik@example.com', 'hashed-password', true);
+
+      INSERT INTO Company (Admin_First_Name, Admin_Last_Name, Company_Name, Full_Schedule, Email, Password)
+      VALUES ('Jane', 'Doe', 'Example Company', true, 'company@email.com', 'hashed');
+      
+      INSERT INTO Schedule (Week_Number, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
+      VALUES (1, true, true, true, true, true, true, true);
+
+      INSERT INTO ScheduleFromEmployee (Schedule_ID, Company_ID)
+      VALUES (1, 1);
+
+      INSERT INTO EmployeesInCompany (Employee_ID, Company_ID)
+      VALUES (1, 1);
+
+      INSERT INTO SuperAdmin (First_Name, Last_Name, Email, Password)
+      VALUES ('Super', 'Admin', 'Admin@bleh.com', 'hashed');
+
     `;
 
     await db.none(insertDataQuery);
@@ -67,7 +130,7 @@ async function run() {
 
     // After the database is created (or if it already exists),
     // create the table
-    await createTable();
+    await createTables();
 
     // After the table is created (or if it already exists),
     // insert the test data
