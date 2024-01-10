d06d2524187b832e1633436a2081da4504fd483d
JoeriDekker <joeridekker2002@gmail.com>
2024-01-10 14:01:31 +0100

    changed name

--- BuurtBoerApp/Models/Employee_Model.tsx
+++ BuurtBoerApp/Models/Employee_Model.tsx
@@ -5,16 +5,16 @@ class Employee {
   lastName: string;
   keepSchedule: boolean;
   companyName: string;
-  full_schedule: boolean;
+  isFullSchedule: boolean;
 
-  constructor(id: number, email: string, firstName: string, lastName: string, keepSchedule: boolean, companyName: string,  full_schedule: boolean) {
+  constructor(id: number, email: string, firstName: string, lastName: string, keepSchedule: boolean, companyName: string,  isFullSchedule: boolean) {
     this.id = id;
     this.email = email;
     this.firstName = firstName;
     this.lastName = lastName;
     this.keepSchedule = keepSchedule;
     this.companyName = companyName;
-    this.full_schedule = full_schedule;
+    this.isFullSchedule = isFullSchedule;
   }
 }
 
