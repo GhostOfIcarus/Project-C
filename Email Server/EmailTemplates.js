866d3c1a2648a26e037b271d22d0cb0d99718083
emoboi01 <1036030@hr.nl>
2024-01-10 15:16:13 +0100

    updated email template
    email now contains english and dutch for user convinice

--- Email Server/EmailTemplates.js
+++ Email Server/EmailTemplates.js
@@ -8,24 +8,53 @@ module.exports = {
 
     forgotPassword: (username, resetPasswordLink) => {
         return `
+            <p><i> For English, please scroll down </i></p>
+            <p>Hallo ${username},</p>
+            <p>Klik alsjeblieft <a href="${resetPasswordLink}">hier</a> om je wachtwoord te resetten.</p>
+            <p>Als je dit niet hebt aangevraagd, negeer deze e-mail dan alsjeblieft en je wachtwoord blijft ongewijzigd.</p>
+            <p>Met vriendelijke groet</p>
+            <p> </p>
+            <p> </P>
+            <p>---------------------------------------------------------------------------------------------------------</p>
+
             <p>Hello ${username},</p>
             <p>Please click <a href="${resetPasswordLink}">here</a> to reset your password.</p>
             <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>
-            <p>Keep Yourself Safe</p>
+            <p>Kind regards</p>
+            
+
         `;
     },
 
     companyRegistration: (username, confirmRegistrationLink) => {
         return `
+            <p><i> For English, please scroll down </i></p>
+            <p>Hallo ${username},</p>
+            <p>Klik alsjeblieft <a href="${confirmRegistrationLink}">hier</a> om de registratie van je bedrijf te starten.</p>
+            <p>Als je dit niet hebt aangevraagd, negeer dan deze e-mail en je registratie blijft onbevestigd.</p>
+            <p>Met vriendelijke groet</p>
+            <p> </p>
+            <p> </P>
+            <p>---------------------------------------------------------------------------------------------------------</p>
+
             <p>Hello ${username},</p>
             <p>Please click <a href="${confirmRegistrationLink}">here</a> to start registering your company.</p>
             <p>If you did not request this, please ignore this email and your registration will remain unconfirmed.</p>
-            <p>Keep Yourself Safe</p>
+            <p>Kind Regards</p>
         `;
     },
 
     employeeInvitation: (firstName, lastName, /*activationKey*/) => {
         return `
+            <p><i> For English, please scroll down </i></p>
+            <p>Hallo ${firstName} ${lastName},</p>
+            <p>Voer alsjeblieft deze activeringscode in de BuurtBoer mobiele app in om je account aan te maken:</p>
+            <p>Als je dit niet hebt aangevraagd, negeer dan deze e-mail en je registratie blijft onbevestigd.</p>
+            <p>Met vriendelijke groet</p>
+            <p> </p>
+            <p> </P>
+            <p>---------------------------------------------------------------------------------------------------------</p>
+
             <p>Hello ${firstName} ${lastName},</p>
             <p>Please enter this activation code in the BuurtBoer mobile app to create your account: </p>
             <p>If you did not request this, please ignore this email and your registration will remain unconfirmed.</p>
@@ -35,6 +64,15 @@ module.exports = {
 
     employeeSecurityKey: (activationKey) => {
         return `
+            <p><i> For English, please scroll down </i></p>
+            <p>Voer alsjeblieft deze activeringscode in de BuurtBoer mobiele app in om te bevestigen dat jij dit bent:</p>
+            <h3>${activationKey}</h3>
+            <p>Als je dit niet hebt aangevraagd, neem dan contact op met een supervisor!</p>
+            <p>Met vriendelijke groet</p>
+            <p> </p>
+            <p> </P>
+            <p>---------------------------------------------------------------------------------------------------------</p>
+
             <p>Hello,</p>
             <p>Please enter this activation code in the BuurtBoer mobile app to confirm that this is you: </p>
             <h3>${activationKey}</h3>
